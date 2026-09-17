ab8ee7fef422e60ee0cb8d7a65c4071bdaf71ba5
htmathl <matheus.domingues126@gmail.com>
2026-09-17 01:59:44 -0300

    db(migrations): organize goose migrations under docker/entre-nos-supabase
        - 00001_schemas.sql: nucleo and financeiro schemas
        - 00002_enums.sql: currency, recurrence, bank, and card enums
        - 00003_tabelas.sql: pessoa and core financial tables
        - 00004_rls_grants.sql: row level security policies and role grants

--- apps/entre-nos-server/migrations/00001_schemas.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-alter database postgres set timezone = 'America/Sao_Paulo';
-
-create schema nucleo;
-create schema financeiro;
-
--- +goose Down
-drop schema financeiro cascade;
-drop schema nucleo cascade;
